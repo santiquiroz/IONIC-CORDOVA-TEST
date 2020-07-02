564f54d33067227b120600f60119cdfdc01e0a98
Santiago Quiroz upegui <squirozu@unal.edu.co>
2020-07-02 11:54:22 -0500

    assignment 4 task 2

--- IONIC/conFusion/src/pages/register/register.ts
+++ IONIC/conFusion/src/pages/register/register.ts
@@ -65,6 +65,27 @@ export class RegisterPage {
     });
   }
 
+  getFromLibrary(){
+    const options: CameraOptions = {
+      quality: 100,
+      targetHeight: 100,
+      targetWidth: 100,
+      correctOrientation: true,
+      allowEdit: true,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.PNG,
+      mediaType: this.camera.MediaType.PICTURE,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+      this.image = imageData;
+      console.log(imageData);
+    },  (err) => {
+      console.log('Error obtaining gallery picture')
+  });
+  }
+
   onSubmit() {
     console.log(this.registerForm.value);
     this.dismiss();
